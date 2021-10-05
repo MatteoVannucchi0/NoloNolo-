c23bf6fa59b2f277c8ffc1471982a248100d0675
matteo00111 <matteo.vannucchi11@gmail.com>
2021-10-05 19:44:31 +0200

    aggiornato il file circleci

---
+++ Backend/public/models/customer.js
@@ -0,0 +1,58 @@
+    const mongoose = require('mongoose');
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const addressSchema = new mongoose.Schema({
+    country: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    zipcode: {
+        type: String,
+        required: true,
+    },
+    streetAddress: {
+        type: String,
+        required: true,
+    }
+})
+
+const customerSchema = new mongoose.Schema({
+    firstname:{
+        type: String,
+        required: false,
+    },
+    lastname:{
+        type: String,
+        required: false,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 8,
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: validateEmail,
+    },
+    dateOfBirth:{
+        type: Date,
+        required: true,
+        default: Date.now(),
+    },
+    address: addressSchema,
+})
+
+module.exports = mongoose.model('Customer', customerSchema);
---
+++ Backend/public/routers/customerRouter.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const customer = require('../models/customer');
+const router = express.Router();
+const Customer = require('../models/customer');
+const Rental = null//require('../models/rental')
+
+router.get('/', async (req, res) => {
+    try {
+        const customer = await Customer.find();
+        res.status(200).json(customer);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+router.post('/', async (req, res) => {
+    let customer = null;
+    try{
+        customer = await Customer({
+            firstname : req.body.firstname,
+            lastname : req.body.lastname,
+            username : req.body.username,
+            password : req.body.password,
+            email : req.body.email,
+            dateOfBirth: req.body.dateOfBirth,
+            address: req.body.address,
+        });
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+
+    try {
+        const newCustomer = await customer.save();
+        res.status(201).json(newCustomer);
+    } catch (error) {
+        res.status(409).json({message: error.message});
+    }
+})
+
+router.get('/:id', getCustomerById, async (req, res) => {
+    res.json(res.customer);
+})
+
+router.delete('/:id', getCustomerById, async (req, res) => {
+    try{
+        let removedCustomer = res.customer
+        await res.customer.remove();
+        res.json(removedCustomer);   //{message: "Customer deleted from the database"});
+    } catch(error){
+        res.status(500).json({message: error.message});
+    }
+})
+
+router.patch('/:id', async (req,res) => {    
+    try{
+        let newCustomer =  await Customer.findOneAndUpdate(req.params.id, req.body, {new: true});
+        if (newCustomer == null)
+            res.status(404).json({message:"Customer not found"})
+
+        res.status(200).json(newCustomer);
+    } catch (error) {
+        res.status(400).json({message: error.message})          //Non so se restituire 400 o 404
+    }
+})
+
+router.get('/:id/rentals', getCustomerById, async (req, res) => {
+    try{
+        let rentals =  await Rental.find({customer: req.params.id})
+        res.status(200).json({rentals})
+    } catch (error) {
+        res.status(400).json({message: error.message})          //Non so se restituire 400 o 404
+    }
+})
+
+router.get('/:id/favorites', async (req, res) => {
+    res.status(404).json({message: 'Non ancora implementanto'});
+})
+
+async function getCustomerById(req, res, next) {
+    let customer;
+    try {
+        customer = await Customer.findById(req.params.id);
+
+        if(customer == null){
+            return res.status(404).json({message: "Customer with id " + req.params.id + " not found on the database"});
+        }
+    } catch (error) {
+        return res.status(400).json({message: error.message});
+    }
+
+    res.customer = customer;
+    next();
+}
+
+module.exports = router;

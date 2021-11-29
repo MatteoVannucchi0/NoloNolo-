90f656e057579aa0a4f25c47386284ab499b2867
matteo00111 <matteo.vannucchi11@gmail.com>
2021-11-29 16:53:28 +0100

    Aggiunta la pagina backoffice

--- public/routers/pageRouter.js
+++ public/routers/pageRouter.js
@@ -2,13 +2,9 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('/backoffice', async (req, res) => {
-    return res.status(200).send("<h1>Pagina BackOffice</h1>")
-})
-
-console.log(path.join(global.frontendDir, '/dashboard'))
-
 router.use('/', express.static(path.join(global.frontendDir, '/dashboard/_nuxt/')));
 router.use('/dashboard', express.static(path.join(global.frontendDir, '/dashboard')));
+router.use('/backoffice', express.static(path.join(global.frontendDir, '/backoffice')));
+
 
 module.exports = router;
